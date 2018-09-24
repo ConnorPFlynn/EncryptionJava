3a9575c8478729b8ae4d8fd833bcabfff568fb2b
Connor Flynn <43014033+ConnorPFlynn@users.noreply.github.com>
2018-09-24 12:15:01 -0700

    Add files via upload

---
+++ Encryption.java
@@ -0,0 +1,9 @@
+//Connor Flynn
+//September 25, 2018
+//Computer Science II, Section II
+//Project I
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Encryption
---
+++ EncryptionDriver.java
@@ -0,0 +1,6 @@
+//Connor Flynn
+//September 25, 2018
+//Computer Science II, Section II
+//Project I
+
+public class EncryptionDriver
