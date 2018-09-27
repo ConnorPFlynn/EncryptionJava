99f4494c97d961d1630f96136ed0caff204e3338
Connor Flynn <43014033+ConnorPFlynn@users.noreply.github.com>
2018-09-26 17:06:47 -0700

    Add files via upload

--- Encryption.java
+++ Encryption.java
@@ -3,7 +3,51 @@
 //Computer Science II, Section II
 //Project I
 
-import java.util.Random;
 import java.util.Scanner;
-
 public class Encryption
+{
+
+    public static char alphabet[]  = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z', ' ' }; 
+
+    public static char key[] = {'r', 'a', 's', 'k', 'o', 'q', 'g', 'f', 'c', 'y', 'j',
+            'x', 'u', 'z', 'n', 'h', 'i', 'p', 'm', 'l', 'd', 'w', 'b', 'e', 't', 'v', ' '}; 
+
+    public static String doEncryption(String x) 
+    {
+        char alist[] = new char[(x.length())];
+
+        for (int i = 0; i < x.length(); i++)
+        {
+            for (int j = 0; j < 27; j++)
+            {
+                if (alphabet[j] == x.charAt(i))
+                {
+                    alist[i] = key[j]; 
+                }
+            }
+        }
+        return (new String(alist)); 
+    }
+
+    public static String doDecryption(String x)
+    {
+        char clist[] = new char[(x.length())];
+
+        for (int i = 0; i < x.length(); i++)
+        {
+            for (int j = 0; j < 27; j++)
+            {
+                if (key[j] == x.charAt(i))
+                {
+                    clist[i] = alphabet[j];
+                }
+            }
+        }
+        return (new String(clist));
+    }
+    
+
+   }
+
--- EncryptionDriver.java
+++ EncryptionDriver.java
@@ -3,4 +3,21 @@
 //Computer Science II, Section II
 //Project I
 
+import java.util.Scanner;
+
 public class EncryptionDriver
+{
+
+public static void main(String[] args)
+    {
+    	Encryption text = new Encryption();
+    
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your text to be encrypted:");
+        String en = text.doEncryption(scan.nextLine().toLowerCase());
+
+        System.out.println("Encrypted text: " + en);
+        String de = text.doDecryption(en);
+        System.out.println("Decrypted text: " + de);
+    }
+   }
